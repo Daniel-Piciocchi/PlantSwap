d4a6ac49ee29f3e5aff25c03b8605d6f112ea4ea
YOUR_USERNAME <daniel_piciocchi@outlook.com>
2023-08-05 18:17:54 +1000

    Fixing incorrect global uploads to individual users

--- src/pages/MyPlantsPage.js
+++ src/pages/MyPlantsPage.js
@@ -8,29 +8,48 @@ const MyPlantsPage = () => {
 
   useEffect(() => {
     const fetchPlants = async () => {
-      const response = await request.get('http://localhost:5001/plants');
-      console.log('Fetched plants:', response.body);  // Log fetched plants
-      setPlants(response.body);
+      try {
+        const token = localStorage.getItem('token'); // Assuming you're storing the token in local storage
+        const response = await request.get('http://localhost:5001/plants/user').set('Authorization', `Bearer ${token}`);
+        console.log('Fetched plants:', response.body);
+        setPlants(response.body);
+      } catch (error) {
+        console.error('Error fetching plants:', error);
+        // Handle the error, maybe set some state to show an error message to the user
+      }
     };
     fetchPlants();
-  }, [plants]);
+  }, []); // Removed plants from the dependency array
 
   const handleDelete = async (id) => {
-    await request.delete(`http://localhost:5001/plants/${id}`);
-    setPlants(plants.filter((plant) => plant._id !== id));
+    try {
+      await request.delete(`http://localhost:5001/plants/${id}`);
+      setPlants(plants.filter((plant) => plant._id !== id));
+    } catch (error) {
+      console.error('Error deleting plant:', error);
+      // Handle the error, maybe set some state to show an error message to the user
+    }
   };
 
   const handleAdd = async () => {
-    const formData = new FormData();
-    formData.append('image', newPlant.image);
-    formData.append('name', newPlant.name);
-    formData.append('description', newPlant.description);
-    const response = await request.post('http://localhost:5001/plants').send(formData);
-    console.log('Server response:', response); // Log the entire server response
-    console.log('Response data:', response.body); // Log the response data
-    setPlants([...plants, response.body]);
-    setNewPlant({ name: '', image: '', description: '' });
-    console.log('Plants after adding:', plants); // Log the plants state after adding a plant
+    try {
+      const token = localStorage.getItem('token'); // Fetch the token again for the POST request
+      const formData = new FormData();
+      formData.append('image', newPlant.image);
+      formData.append('name', newPlant.name);
+      formData.append('description', newPlant.description);
+      const response = await request.post('http://localhost:5001/plants')
+                                    .set('Authorization', `Bearer ${token}`) // Include the token in the request header
+                                    .send(formData);
+      console.log('Server response:', response);
+      console.log('Response data:', response.body);
+      setPlants([...plants, response.body]);
+      setNewPlant({ name: '', image: '', description: '' });
+      console.log('Plants after adding:', plants);
+    } catch (error) {
+      console.error('Error adding plant:', error);
+      // Handle the error, maybe set some state to show an error message to the user
+    }
   };
 
   const handleImageUpload = (event) => {
@@ -44,7 +63,6 @@ const MyPlantsPage = () => {
         {plants.map((plant) => (
           <div className="plant-listing" key={plant._id}>
             <img src={`http://localhost:5001/uploads/${plant.image}`} alt={plant.name} />
-
             <h3>{plant.name}</h3>
             <p>{plant.description}</p>
             <div className="plant-buttons">
